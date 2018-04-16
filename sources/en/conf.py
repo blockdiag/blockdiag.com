7a943aaf4d2fbc4b5d9bb99e9a45b29c99bf3583
Takeshi KOMIYA <i.tkomiya@gmail.com>
2018-04-16 23:50:44 +0900

    Add "sponsors for @tk0miya" to sidebar

--- sources/en/conf.py
+++ sources/en/conf.py
@@ -138,7 +138,9 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = {
+    '**': ['localtoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html', 'donation.html'],
+}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
